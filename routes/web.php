74da5cc1838567d530bc334d1c834eecd1db2327
Aditya Pramono <adityapramono96@gmail.com>
2024-01-14 21:27:44 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+// Membuat Home dashboar
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard');
+    })->name('home');
+    Route::resource('user', UserController::class);
+    //category
+    Route::resource('category', \App\Http\Controllers\CategoryController::class);
+    //category
+    Route::resource('product', \App\Http\Controllers\ProductController::class);
+});
+
+
